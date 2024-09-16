c228b3c2c85b618f769e9035b0ae9861dbafc421
Nuhu Lawal <nuhulawal20@gmail.com>
2024-09-16 15:15:33 +0100

    my daily challenge

---
+++ 8kyu/Cockroach-function-to-convert-kph-to-cps.js
@@ -0,0 +1,14 @@
+
+
+// The cockroach is one of the fastest insects. 
+//Write a function which takes its speed in km per hour and returns it in cm per second, rounded down 
+//to the integer (= floored).
+
+//my Solutions:
+
+function cockroachSpeed(s) {
+    return Math.floor(s/0.036)
+  }
+
+
+const cockroachSpeed = s => Math.floor(s / 0.036);
