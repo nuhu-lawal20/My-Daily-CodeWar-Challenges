cc88b8f2ef1fe194f403cb63df9ec14fc9caa525
Nuhu Lawal <nuhulawal20@gmail.com>
2024-10-10 16:35:29 +0100

    my dialy code

---
+++ 8kyu/5-withouth-numbers.js
@@ -0,0 +1,15 @@
+// Description:
+// Write a function that always returns 5
+// Sounds easy right? Just bear in mind that you can't use any of the following characters: 0123456789*+-/
+
+//My solutions:
+//1)
+function unusualFive() {
+    let arr = ['a','b','c','d','e'];
+     return arr.length
+    }
+
+//2)
+function unusualFive() {
+     return 'lover'.length
+    }
