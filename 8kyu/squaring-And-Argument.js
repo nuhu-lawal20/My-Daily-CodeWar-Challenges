a138444058acaffd2bd869c124eebaf020a3b3e1
Nuhu Lawal <nuhulawal20@gmail.com>
2024-10-17 12:25:03 +0100

    my daily code

---
+++ 8kyu/squaring-And-Argument.js
@@ -0,0 +1,11 @@
+// Description:
+// Now you have to write a function that takes an argument and returns the square of it.
+
+//Solution:
+//1)
+const square = (num) => (num**2)
+
+//2)
+function square2(num){
+    return num*num
+}
