756ab06a61beecb83dd4eb9eb5c7be15849f5a6b
Nuhu Lawal <nuhulawal20@gmail.com>
2024-11-18 17:09:08 +0100

    daily code challenges

---
+++ 8kyu/function-helloWorld.js
@@ -0,0 +1,10 @@
+// Description:
+// Make a simple function called greet that returns the most-famous "hello world!".
+
+//1)
+function greet(){
+    return `hello world!`;
+  };
+
+//2)
+const greet = () => "hello world!";
